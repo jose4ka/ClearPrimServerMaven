4f916a4ad8b08228712ee1c892b71d506c687eae
dmitriy <rabowvdk125@gmail.com>
2020-12-16 00:53:25 +1000

    Initial commit

---
+++ src/main/java/com/example/ClearPrimServerMaven/ClearPrimServerMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClearPrimServerMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearPrimServerMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearPrimServerMavenApplication.class, args);
+	}
+
+}
